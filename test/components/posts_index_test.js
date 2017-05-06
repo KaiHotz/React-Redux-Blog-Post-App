3e1b24115fba4df6a7f42a6d7f1a3e9e1db84abc
KaiHotz <kaihotz@me.com>
2017-05-06 21:50:03 +0200

    Dependencies Update

--- test/components/posts_index_test.js
+++ test/components/posts_index_test.js
@@ -1,11 +1,11 @@
 import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+import PostsIndex from '../../src/components/posts_index';
 
-describe('App' , () => {
+describe('Posts Index' , () => {
   let component;
 
   beforeEach(() => {
-    component = renderComponent(App);
+    component = renderComponent(PostsIndex);
   });
 
   it('renders something', () => {
